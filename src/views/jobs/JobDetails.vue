e5b534bf090eb488cec006fbcebfb0f2de72b047
ostafiichuk <issimo333@gmail.com>
2023-01-02 17:16:30 +0200

    feat: fetching data from json-server

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,26 @@
 <template>
-    <h1>Job details page</h1>
-    <p>ID {{ id }}</p>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p>ID {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>Loading job details...</div>
 </template>
 
 <script>
 export default {
     props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+        return {
+            job: null
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then(response => response.json())
+            .then(data => this.job = data)
+            .catch(e => console.log(e.message))
+    }
 }
 </script>
 
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,9 +1,14 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-            <h2>{{ job.title }}</h2>
-        </router-link>
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+                <h2>{{ job.title }}</h2>
+            </router-link>
+        </div>
+    </div>
+    <div v-else>
+        <p>Loading jobs...</p>
     </div>
 </template>
 
@@ -11,12 +16,14 @@
 export default {
     data() {
         return {
-            jobs: [
-                { title: 'UX designer', id: 1, details: 'lorem' },
-                { title: 'Web developer', id: 2, details: 'lorem' },
-                { title: 'Vue developer', id: 3, details: 'lorem' }
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+            .then(response => response.json())
+            .then(data => this.jobs = data)
+            .catch(e => console.log(e.message))
     }
 }
 </script>
