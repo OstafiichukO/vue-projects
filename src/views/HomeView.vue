01563b02301f8fa0ed69e71098786d96dfc74ddb
ostafiichuk <issimo333@gmail.com>
2023-01-03 11:39:42 +0200

    feat: reactive and ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++">increase age</button>
-    <input type="text" v-model="name">
-  </div>
+    <div class="home">
+        <h1>Home</h1>
+        <h2>Refs</h2>
+        <p>{{ userOne.name }} - {{ userOne.age }}</p>
+        <button @click="updateUserOne">Update user one</button>
+        <h2>Reactive</h2>
+        <p>{{ userTwo.name }} - {{ userTwo.age }}</p>
+        <button @click="updateUserTwo">Update user two</button>
+    </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { ref, reactive } from '@vue/reactivity';
 
 
 export default {
-  name: 'HomeView',
-  setup() {
-    const name = ref('Alex');
-    const age = ref(40);
+    name: 'HomeView',
+    setup() {
+        const userOne = ref({ name: 'Alex', age: 30 });
+        const userTwo = reactive({ name: 'Bruno', age: 36 });
 
-    // const p = ref(null)
 
-    const handleClick = () => {
-      name.value = 'Luigi'
-      age.value = 30
-    };
-    return { name, age, handleClick }
-  }
+        const updateUserOne = () => {
+            userOne.value.age = 35
+        }
+        const updateUserTwo = () => {
+            userTwo.age = 46
+        }
+        return { userOne, userTwo, updateUserOne, updateUserTwo }
+    }
 }
 </script>
