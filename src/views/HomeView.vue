7321a59c238d63c76c6ae91b4af6fa1586a2f795
ostafiichuk <issimo333@gmail.com>
2023-01-03 11:05:48 +0200

    feat: refs

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+
+
+export default {
+  name: 'HomeView',
+  setup() {
+    let name = 'Alex';
+    let age = 40;
+
+    const p = ref(null)
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+    };
+    return { name, age, handleClick, p }
+  }
+}
+</script>
