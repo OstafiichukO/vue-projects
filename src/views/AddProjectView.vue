2c905cc18a428084f9398c1b558472c0ea4d85a4
ostafiichuk <issimo333@gmail.com>
2023-01-05 15:38:56 +0200

    feat: navbar

--- src/views/AddProjectView.vue
+++ src/views/AddProjectView.vue
@@ -18,7 +18,18 @@ export default {
     },
     methods: {
         handleSubmit() {
-            console.log('title:', this.title, 'details:', this.details)
+            let project = {
+                title: this.title,
+                details: this.details,
+                complete: false
+            }
+            fetch('http://localhost:3000/projects', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(project)
+            }).then(() => {
+                this.$router.push('/')
+            }).catch(e => console.log(e))
         }
     }
 }
