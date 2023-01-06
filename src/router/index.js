a48db13fd416a894afbe9fa216e119ce12ad11ff
ostafiichuk <issimo333@gmail.com>
2023-01-06 10:22:03 +0200

    feat: edit project route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AddProject from '../views/AddProjectView.vue'
+import EditProject from '../views/EditProjectView.vue'
 
 const routes = [
   {
@@ -12,6 +13,12 @@ const routes = [
     path: '/add',
     name: 'AddProject',
     component: AddProject
+  },
+  {
+    path: '/projects/:id',
+    name: 'EditProject',
+    component: EditProject,
+    props: true
   }
 ]
 
